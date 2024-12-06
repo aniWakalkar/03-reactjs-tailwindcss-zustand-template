20fc7bd05470b046698928c2cdd7d40afe573a51
master <aniketwakalkar23@gmail.com>
2024-12-06 21:39:34 +0530

    Second Navigation code included

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./components/About";
 import Counter from "./components/Counter";
 import Home from "./components/Home";
-import Navbar from "./components/Navbar";
+import NavbarBar from "./components/NavbarBar";
+// import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="h-full flex flex-col">
       <Router>
-        <Navbar />
+        {/* <Navbar /> */}
+        <NavbarBar />
         <div className="flex-grow overflow-auto">
           <Routes>
             <Route path="/" element={<Home />} />
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div className="h-[85vh] flex items-center justify-center bg-gray-100">
-            About
+        <div className="h-[85vh] w-[100%] flex items-center justify-center bg-gray-100">
+            <h1>About</h1>
         </div>
     );
 }
